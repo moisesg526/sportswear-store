4d2ac9b552fa6d16744a6e221241a7466b44a690
moises gomez <moisesg526@yahoo.com>
2023-03-20 00:01:48 -0400

    Added timer to Free shipping component

--- src/components/FreeShipping.js
+++ src/components/FreeShipping.js
@@ -1,8 +1,24 @@
 import "../styles/FreeShipping.css";
 function FreeShipping() {
+  let countDownDate = new Date("April 1, 2023 12:00:00"). getTime();
+  let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(distance / (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML ="[" + days + "d " + hours + "h " + minutes + "m " + seconds + "s ]";
+    if(distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "Free Shipping ended"
+    }
+  }, 1000)
   return (
     <div className="ribbon">
-      <div className="inner-ribbon-left">HURRY! ENDS IN : </div>
+      <div className="inner-ribbon-left">HURRY! ENDS IN : <p id="countdown"></p></div>
       <div className="inner-ribbon-right">
         <div className="free right">
           {" "}
