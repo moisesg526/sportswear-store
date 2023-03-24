c159e1e78daf5df057c5987633bfcbd0012e1295
moises gomez <moisesg526@yahoo.com>
2023-03-23 22:04:07 -0400

    added button to signup popup

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,9 @@ function Form(props) {
   return props.trigger ? (
     <div className="form">
       <div className="inner-form">
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          X
+        </button>
         <h2>Save 10% instantly</h2>
         <h4>
           Get 10% off your first order when you enter your information below.
@@ -11,9 +14,22 @@ function Form(props) {
         <h6>*Offer valid to new customers only</h6>
         <div className="form-contents">
           <form action="">
-            <input className="form-inputs" type="text" placeholder="Email Address" required />
-            <input className="form-inputs" type="text" placeholder="First Name" />
-            <input className="form-inputs" type="text" placeholder="Last Name" />
+            <input
+              className="form-inputs"
+              type="text"
+              placeholder="Email Address"
+              required
+            />
+            <input
+              className="form-inputs"
+              type="text"
+              placeholder="First Name"
+            />
+            <input
+              className="form-inputs"
+              type="text"
+              placeholder="Last Name"
+            />
           </form>
           <h6>By signing up you agree to receive our monthly emails.</h6>
         </div>
